4445a0e49dc2a34fdb180b35b168b8fddc7fa34d
Invalidid404 <karl2003@naver.com>
2017-06-13 00:57:40 +0900

    0613. 다중선형회귀(2), 데이터 시각화(1)
    나는 3차원 평면을 원한다 젤나가여

--- pyreg.py
+++ pyreg.py
@@ -1,6 +1,9 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+
 import sympy as sp
 from anal_excel import xlanal
 
@@ -10,45 +13,53 @@ class LinearRegression:  # 선형회귀 클래스
 
         self.dataset = dataset
 
-        self.gradient = 0  # 기울기 (ax+b에서 a)
-        self.intercept = 0  # x 절편 (ax+b 에서 b)
+        self.coeff = ()
 
-    def draw(self, graph=0):  # 0 : 점만 표현, 1 : 그래프만 표현 2 : 점과 그래프를 함께 표현
-        if graph is 0:
-            plt.plot(self.x_data, self.y_data, 'ro')  # 산포도
-            plt.xlabel('x')
-            plt.ylabel('y')
+    def draw(self, graph=0, dimension=1):  # 0 : 점만 표현, 1 : 그래프만 표현 2 : 점과 그래프를 함께 표현
+        if dimension is 1:
+            if graph is 0:
+                plt.plot([self.dataset[i][0] for i in range(self.p)], [self.dataset[i][1] for i in range(self.p)], 'ro')  # 산포도
+                plt.xlabel('x')
+                plt.ylabel('y')
+                plt.show()
+
+            elif graph is 1:  # 회귀식 그래프
+                x_data = [self.dataset[i][0] for i in range(self.p)]
+                plt.plot(x_data, [self.coeff[1] * x_data[i] + self.coeff[1] for i in range(self.p)], '-')
+                plt.xlabel('x')
+                plt.ylabel('y')
+                plt.show()
+
+            elif graph is 2:
+                x_data = [self.dataset[i][0] for i in range(self.p)]
+                y_data = [self.dataset[i][1] for i in range(self.p)]
+                plt.plot(x_data, y_data, 'ro')  # 회귀식 그래프와 산포도를 함께 표현
+                plt.plot(x_data, [self.coeff[0] * x_data[i] + self.coeff[1] for i in range(self.p)], '-')
+                plt.xlabel('x')
+                plt.ylabel('y')
+                plt.show()
+        elif dimension is 2:
+            x = np.arange(0, 10, 0.1)  # points in the x axis
+            y = np.arange(0, 10, 0.1)  # points in the y axis
+            z = [self.coeff[0]*x[i] + self.coeff[1]*y[i]+self.coeff[2] for i in range(self.p)]  # points in the z axis
+
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d')
+            ax.plot(x, y, z, 'b-')
             plt.show()
 
-        elif graph is 1:  # 회귀식 그래프
-            plt.plot(self.x_data, [self.gradient * self.x_data[i] + self.intercept for i in range(self.p)])
-            plt.xlabel('x')
-            plt.ylabel('y')
-            plt.show()
-
-        elif graph is 2:
-            print(self.gradient, self.intercept)
-            plt.plot(self.x_data, self.y_data, 'ro')  # 회귀식 그래프와 산포도를 함께 표현
-            plt.plot(self.x_data, [self.gradient * self.x_data[i] + self.intercept for i in range(self.p)])
-            plt.xlabel('x')
-            plt.ylabel('y')
-            plt.show()
-
-    def regress(self, dimension=2, a=0.005, tolerance = 0.00001):
+    def regress(self, dimension=1, a=0.005, tolerance = 0.00001):
         var = sp.symbols(' '.join([chr(ord('A')+i) for i in range(dimension)]))
         sym = sp.symbols(' '.join([chr(ord('a')+i) for i in range(dimension+1)]))
-
-        if type(var) is type(sp.symbols('a')):
+        if type(var) is not tuple:
             var = var,
-            sym = sym,
 
         f = sum([ sym[i] * var[i] for i in range(dimension)]) + sym[-1]
-        print(type(f))
 
-        cost = (1 / (2 * self.p))\
-               * sum([(self.dataset[i][-1] - f.subs({var[l]:self.dataset[i][l] for l in range(dimension)})) ** 2 for i in range(self.p)])
+        cost = sp.expand((1 / (2 * self.p))\
+               * sum([(self.dataset[i][-1] - f.subs({var[l]:self.dataset[i][l] for l in range(dimension)})) ** 2 for i in range(self.p)]))
 
-        print(self.argmin(dimension+1, cost, sym))
+        self.coeff = self.argmin(dimension+1, cost, sym)
 
     def argmin(self, k, func, sym, a=0.005,tolerance =0.00001):  # 경사감소법 옵티마이저
         v = np.array([0 for _ in range(k)]).T
@@ -68,25 +79,13 @@ def main():
     p_set = []
     for i in range(p):  # 산포도 생성
         x = np.random.normal(0.0, 0.5)
-        y = 0.3*x+0.5
-        p_set.append([x, y])
+        y = np.random.normal(0.0, 0.5)
+        z = 3*x + 0.5*y + np.random.normal(0.0, 0.5)
+        p_set.append([x, y, z])
 
     s = LinearRegression(p_set)
-    #s.draw()  # 그래프 그리기
-    s.regress(1)
-    #s.draw(0)
-    #s.draw(1)
-    #s.draw(2)
+    s.regress(2)
+    print(s.coeff)
+    s.draw(1, 2)
 
 main()
-
-args = sys.argv[1:]
-if args[1]=='test':
-    main()
-
-elif args[1]=='excel':
-    s = LinearRegression(xlanal(args[2]))
-    s.regress(1)
-
-elif args[2]=='run':
-    pass
