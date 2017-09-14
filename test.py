0fb822bdc83b38c2eb1a06cc0f45b34dd5e6a3fc
Invalidid404 <karl2003@naver.com>
2017-09-15 08:16:53 +0900

    0915. 게시용 설명표

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
-import numpy as np
-
-print()
+card = list(range(0, 6))
+case = 0
+for i in card:
+    for l in card:
+        if l is not i:
+            left = [c for c in card if (c is not l and c is not i)and(c is 0 or c is 5)]
+            case += len(left)
+print(case)
